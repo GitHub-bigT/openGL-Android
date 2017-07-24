4b35aab2b9625f7fda701222394c5c396056a743
GitHub-bigT <564241248@qq.com>
2017-07-24 21:27:43 +0800

    know init

--- app/src/main/java/ricoh/opengljava/MyRender.java
+++ app/src/main/java/ricoh/opengljava/MyRender.java
@@ -17,29 +17,58 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class MyRender implements GLSurfaceView.Renderer {
     private static final String TAG = "ooopppp";
-    private Triangle mTriangle;
-
+    private Triangle mTriangle1;
+    private Triangle mTriangle2;
+    private Triangle mTriangle3;
+    float[] coord1 = {
+            0f, 1f, 0.0f,   //上
+            -0.7f, 0f, 0.0f,   // 左下
+            0.7f, 0f, 0.0f    // 右下
+    };
+    float[] color1 = { 1f, 0f, 0f ,0f};
+    float[] coord2 = {
+            0f, 1f, 0.0f,   //上
+            0.7f, 0f, 0.0f,   // 左下
+            0.9f, 0.3f, 0.0f    // 右下
+    };
+    float[] color2 = { 0f, 1f, 0f ,0f};
+    float[] coord3 = {
+            0f, 1f, 0.0f,   //上
+            -0.7f, 0f, 0.0f,   // 左下
+            -0.5f, 0.3f, 0.0f    // 右下
+    };
+    float[] color3 = { 0f, 0f, 1f ,0f};
+    //当创建GLSurfaceView时被调用，只调用一次．
+    // 在这个方法中执行只发生一次的动作，比如设置OpenGL环境参数或初始化OpenGL图形对象
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         Log.i(TAG, "surface create");
         //背景颜色
 //        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        mTriangle = new Triangle();
+//        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        mTriangle1 = new Triangle();
+        mTriangle2 = new Triangle();
+        mTriangle3 = new Triangle();
     }
 
+    //当GLSurfaceView几何体改变时系统调用此方法，
+    //比如GLSurfaceView的大小改变或设备屏幕的方向改变．
+    // 使用此方法来响应GLSurfaceView容器的变化
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         Log.i(TAG, "surface change");
         Log.i(TAG, "width :" + width + " height:" + height);
-        GLES20.glViewport(0, 0, width, height);
+//        GLES20.glViewport(0, 0, width, height);
     }
 
+    //系统在每次重绘GLSurfaceView时调用此方法．此方法是绘制图形对象的主要的执行点
     @Override
     public void onDrawFrame(GL10 gl) {
         Log.i(TAG, "onDrawFrame");
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        mTriangle.draw();
+//        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        mTriangle1.draw(coord1,color1);
+        mTriangle2.draw(coord2,color2);
+        mTriangle3.draw(coord3,color3);
     }
 
     /**
@@ -59,22 +88,25 @@ public class MyRender implements GLSurfaceView.Renderer {
                         "}";
         private int mProgram = GLES20.glCreateProgram();
         private int mPositionHandler;
-        private float trianglecoords[] = {
-                0.0f, 1f, 0.0f,   //上
-                -1f, 0f, 0.0f,   // 左下
-                1f, -1f, 0.0f    // 右下
-        };
+        private float trianglecoords[] ;
         private FloatBuffer vertexBuffer;
         //数组中每个顶点的坐标数
         int COORDS_PER_VERTEX = 3;
-        private final int vertexCount = trianglecoords.length / COORDS_PER_VERTEX;
         private final int vertexStride = COORDS_PER_VERTEX * 4; // bytes per vertex
         private int mColorHandle;
-        float color[] = { 1f, 0f, 0f, 1.0f };
+        float color[] ;
 
-        public Triangle() {
-            //初始化字节缓冲区
+
+        public void draw(float coords[],float c[]) {
+            this.trianglecoords  = coords;
+            this.color = c;
+            int vertexCount = trianglecoords.length / COORDS_PER_VERTEX;
+            /*
             //为三角形初始化顶点字节缓冲区
+            //不能直接使用FloatBuffer ： Must use a native order direct Buffer
+            //GLES20.glVertexAttribPointer(mPositionHandler, COORDS_PER_VERTEX,GLES20.GL_FLOAT,false,vertexStride,vertexBuffer);
+            //最后一个参数有限制
+            */
             ByteBuffer bb = ByteBuffer.allocateDirect(trianglecoords.length * 4);
 //            for (int i = 0; i < trianglecoords.length; i++) {
 //                Log.i(TAG,"float[] "+trianglecoords[i]);
@@ -88,6 +120,8 @@ public class MyRender implements GLSurfaceView.Renderer {
 //            for (int i = 0 ; i < bb.array().length ; i++) {
 //                Log.i(TAG,"bb order  "+bb.array()[i]);
 //            }
+//            vertexBuffer = FloatBuffer.allocate(trianglecoords.length);
+
             Log.i(TAG, "program: " + mProgram);
             vertexBuffer = bb.asFloatBuffer();
             vertexBuffer.put(trianglecoords);
@@ -104,24 +138,25 @@ public class MyRender implements GLSurfaceView.Renderer {
             int fragmentShader = MyRender.loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
             Log.i(TAG, "vertex shader: " + vertexShader);
             Log.i(TAG, "fragment shader: " + fragmentShader);
-            mProgram = GLES20.glCreateProgram();
+//            mProgram = GLES20.glCreateProgram();
             GLES20.glAttachShader(mProgram, vertexShader);
             GLES20.glAttachShader(mProgram, fragmentShader);
-            //创建opengl程序的可执行文件
+            //创建opengl程序的可执行文件/
             GLES20.glLinkProgram(mProgram);
-        }
 
-        public void draw() {
             GLES20.glUseProgram(mProgram);
             mPositionHandler = GLES20.glGetAttribLocation(mProgram, "vPosition");
+            Log.i(TAG , "mPositionHandler: "+mPositionHandler);
             GLES20.glEnableVertexAttribArray(mPositionHandler);
-            GLES20.glVertexAttribPointer(mPositionHandler, COORDS_PER_VERTEX,GLES20.GL_FLOAT,false,vertexStride,vertexBuffer);
+            GLES20.glVertexAttribPointer(mPositionHandler, COORDS_PER_VERTEX,GLES20.GL_FLOAT,true,vertexStride,vertexBuffer);
             //设置颜色
             mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+            Log.i(TAG , "mColorHandle: "+mColorHandle);
             GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+//            GLES20.glUniform3fv(mColorHandle, 1, color, 0);
             GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
             // Disable vertex array
-            GLES20.glDisableVertexAttribArray(mPositionHandler);
+//            GLES20.glDisableVertexAttribArray(mPositionHandler);
         }
     }
 
