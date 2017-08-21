6427f3b9a9c1d31332a2439e789e95c4d46857b4
GitHub-bigT <564241248@qq.com>
2017-08-21 12:35:12 +0800

    init glfw

--- opengl-pc/opengl-study/opengl-study/Source.cpp
+++ opengl-pc/opengl-study/opengl-study/Source.cpp
@@ -1,41 +1,59 @@
 #define GLEW_STATIC
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
+#include <iostream>
 
-int main(void)
-{
-	GLFWwindow* window;
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
-
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
+int main(){
+	glfwInit();
+	//设置opengl的主版本和次版本（3.3）
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	//设置窗口
+	GLFWwindow* window = glfwCreateWindow(800,600,"bigT-OpenGL",nullptr,nullptr);
+	if (window == nullptr)
 	{
+		std::cout << "failed to create GLFW window" << std::endl;
+		//释放资源
 		glfwTerminate();
 		return -1;
 	}
-
-	/* Make the window's context current */
+	//通知GLFW将窗口的上下文设置为当前线程的主上下文
 	glfwMakeContextCurrent(window);
-
-	/* Loop until the user closes the window */
+	//
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+	//获取glfw窗口的width和height
+	int width, height;
+	glfwGetFramebufferSize(window,&width,&height);
+	std::cout << "width:" << width << "  height:" << height << std::endl;
+	glViewport(0,0,width,height);
+	//设置键盘监听事件
+	glfwSetKeyCallback(window,key_callback);
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		auto glewError = glewInit();
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
+		//检查触发事件
+		//键盘输入、鼠标移动等
 		glfwPollEvents();
+		//双缓存技术
+		glfwSwapBuffers(window);
 	}
-
+	//释放资源
 	glfwTerminate();
 	return 0;
 }
+
+void key_callback(GLFWwindow* window, int key , int scancode , int action , int mode){
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		std::cout << "account press ESC" << std::endl;
+		glfwSetWindowShouldClose(window,GL_TRUE);
+	}
+};
