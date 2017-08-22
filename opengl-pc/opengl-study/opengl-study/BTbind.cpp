c37fe885511ddcd27735a2d99bc7bf19701158c5
GitHub-bigT <564241248@qq.com>
2017-08-22 10:55:34 +0800

    vao vbo

--- opengl-pc/opengl-study/opengl-study/BTbind.cpp
+++ opengl-pc/opengl-study/opengl-study/BTbind.cpp
@@ -11,8 +11,10 @@ void BTVaoVbo::initVaoVbo(){
 			0.0f, 1.0f, 0.0f, 1.0f,
 			0.0f, 0.0f, 1.0f, 1.0f
 	};
+	//1.绑定VBO
 	glGenBuffers(NumVBOIds, VBOs);
 	glBindBuffer(GL_ARRAY_BUFFER,VBOs[TriangleBO]);
+	//2.将数据传给显存当中的ARRAY_BUFFER缓冲块中
 	glBufferData(GL_ARRAY_BUFFER,sizeof(triangleVertex)+sizeof(triangleColor),NULL,GL_STATIC_DRAW);
 
 	glBufferSubData(GL_ARRAY_BUFFER,0,sizeof(triangleVertex),triangleVertex);
@@ -20,17 +22,20 @@ void BTVaoVbo::initVaoVbo(){
 
 	GLboolean b = glIsBuffer(VBOs[TriangleBO]);
 	printf("triangle vbo bind: %d\n", b);
-
+	//(ig75icd32.dll) 崩溃
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//1.绑定VAO
 	glGenVertexArrays(NumVaoIds, VAOs);
 	glBindVertexArray(VAOs[TriangleAO]);
-
 	GLboolean b1 = glIsVertexArray(VAOs[TriangleAO]);
 	printf("triangle vao bind: %d\n", b1);
-
+	//2.设置顶点属性指针
 	glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, 0);
 	glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0, (const void*)sizeof(triangleVertex));
 	glEnableVertexAttribArray(vPosition);
 	glEnableVertexAttribArray(vColor);
+	//3.解绑vao,避免在其他地方错误的配置
+	glBindVertexArray(0);
 }
 
 void BTVaoVbo::drawArrays(){
@@ -38,5 +43,6 @@ void BTVaoVbo::drawArrays(){
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBindVertexArray(VAOs[TriangleAO]);
 	glDrawArrays(GL_TRIANGLE_STRIP,0,3);
+	glBindVertexArray(0);
 	glFlush();
 }
