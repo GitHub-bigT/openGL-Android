24ea3aa11429b6dadc14df2dadee72e1516518f4
GitHub-bigT <564241248@qq.com>
2017-08-31 07:36:40 +0800

    camera

--- opengl-pc/opengl-study/opengl-study/Source.cpp
+++ opengl-pc/opengl-study/opengl-study/Source.cpp
@@ -12,7 +12,7 @@ float alpha = 0.0f;
 float rotateAngle = 0.0f;
 enum {triangle = 1 , ball = 2};
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -3.0f);
+glm::vec3 cameraFront;
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 //¸©Ñö½Ç Æ«º½½Ç
 GLfloat pitch = 0.0f;
