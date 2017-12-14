269e97b308b08414226ae034a74b22e9d415dd8c
GitHub-bigT <564241248@qq.com>
2017-12-14 10:18:13 +0800

    init plante environment

--- afresh/instancing/instancing/main.cpp
+++ afresh/instancing/instancing/main.cpp
@@ -178,9 +178,10 @@ void initTriangle()
 	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 100, &translations[0], GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glEnableVertexAttribArray(2);
+	
 	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(2);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glVertexAttribDivisor(2, 1);
 	glBindVertexArray(0);
