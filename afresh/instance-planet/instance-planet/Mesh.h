40f15aeaf65653f31d6d5de0a78ec5de65175045
GitHub-bigT <564241248@qq.com>
2018-02-23 09:33:31 +0800

    delete model.cpp and mesh.cpp

--- afresh/instance-planet/instance-planet/Mesh.h
+++ afresh/instance-planet/instance-planet/Mesh.h
@@ -25,11 +25,48 @@ public:
 	std::vector<Vertex> verVec;
 	std::vector<Texture> texVec;
 	std::vector<GLuint> indexVec;
-	Mesh(std::vector<Vertex> verVec, std::vector<Texture> texVec, std::vector<GLuint> indexVec);
-	~Mesh();
-	void Draw(Shader shader);
+	Mesh(std::vector<Vertex> verVec, std::vector<Texture> texVec, std::vector<GLuint> indexVec)
+	{
+		this->verVec = verVec;
+		this->texVec = texVec;
+		this->indexVec = indexVec;
+
+		setupMesh();
+	}
+
+	~Mesh()
+	{
+
+	}
+
+	void Draw(Shader shader)
+	{
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, indexVec.size(), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+	}
 
 private:
 	GLuint VAO, VBO, EBO;
-	void setupMesh();
+	void setupMesh()
+	{
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+
+		glBindVertexArray(VAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * verVec.size(), &verVec[0], GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * indexVec.size(), &indexVec[0], GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+		//glEnableVertexAttribArray(1);
+		//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+		//glEnableVertexAttribArray(2);
+		//glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoord));
+	}
 };
--- afresh/instance-planet/instance-planet/Model.h
+++ afresh/instance-planet/instance-planet/Model.h
@@ -17,16 +17,100 @@
 class Model
 {
 public:
-	Model();
-	void Draw(Shader shader);
-	void loadModel(std::string path);
-	~Model();
+	Model()
+	{
+	}
+
+	void Draw(Shader shader)
+	{
+		for (int i = 0; i < meshes.size(); i++)
+		{
+			meshes.at(i).Draw(shader);
+		}
+	}
+
+	void loadModel(std::string path)
+	{
+		Assimp::Importer import;
+		const aiScene *scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+		{
+			std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
+			return;
+		}
+		directory = path.substr(0, path.find_last_of('/'));
+
+		processNode(scene->mRootNode, scene);
+	}
+
+	~Model()
+	{
+	}
 
 private:
 	std::vector<Mesh> meshes;
 	std::string directory;
 
-	void processNode(aiNode *node, const aiScene *scene);
-	Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+	void processNode(aiNode *node, const aiScene *scene)
+	{
+		for (int i = 0; i < node->mNumMeshes; i++)
+		{
+			aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+			meshes.push_back(processMesh(mesh, scene));
+		}
+		for (int i = 0; i < node->mNumChildren; i++)
+		{
+			processNode(node->mChildren[i], scene);
+		}
+	}
+
+	Mesh processMesh(aiMesh *mesh, const aiScene *scene)
+	{
+		//vertex
+		std::vector<Vertex> verVec;
+		std::vector<GLuint> indexVec;
+		std::vector<Texture> texVec;
+
+		for (int i = 0; i < mesh->mNumVertices; i++)
+		{
+			Vertex vertex;
+			glm::vec3 vector;
+			vector.x = mesh->mVertices[i].x;
+			vector.y = mesh->mVertices[i].y;
+			vector.z = mesh->mVertices[i].z;
+			vertex.Position = vector;
+
+			vector.x = mesh->mNormals[i].x;
+			vector.y = mesh->mNormals[i].y;
+			vector.z = mesh->mNormals[i].z;
+			vertex.Normal = vector;
+
+			if (mesh->mTextureCoords[0]) // 网格是否有纹理坐标？
+			{
+				glm::vec2 vec;
+				vec.x = mesh->mTextureCoords[0][i].x;
+				vec.y = mesh->mTextureCoords[0][i].y;
+				vertex.TexCoord = vec;
+			}
+			else
+				vertex.TexCoord = glm::vec2(0.0f, 0.0f);
+
+			verVec.push_back(vertex);
+		}
+
+		//index
+		for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+		{
+			aiFace face = mesh->mFaces[i];
+			for (unsigned int j = 0; j < face.mNumIndices; j++)
+				indexVec.push_back(face.mIndices[j]);
+		}
+
+		//material
+
+
+		return Mesh(verVec, texVec, indexVec);
+	}
+	
 };
 
