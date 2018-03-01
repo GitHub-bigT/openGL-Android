9bda333ecdb6b6be46bd6691d43fd55d3e77f3a4
GitHub-bigT <564241248@qq.com>
2018-03-01 09:30:17 +0800

    add geometry explode project

---
+++ afresh/geometry-explode/simple-camera/Model.h
@@ -0,0 +1,196 @@
+#pragma once
+#include <string>
+#include <vector>
+
+#include <glad/glad.h> 
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+
+#include <stb_image.h>
+
+#include "Shader.h"
+#include "Mesh.h"
+
+unsigned int TextureFromFile(const char *path, const std::string &directory, bool gamma = false);
+
+class Model
+{
+public:
+	std::vector<Mesh> meshes;
+
+	Model(std::string path)
+	{
+		Assimp::Importer import;
+		const aiScene *scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+		{
+			std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
+			return;
+		}
+		directory = path.substr(0, path.find_last_of('/'));
+
+		processNode(scene->mRootNode, scene);
+	}
+
+	void Draw(Shader shader)
+	{
+		for (int i = 0; i < meshes.size(); i++)
+		{
+			meshes.at(i).Draw(shader);
+		}
+	}
+
+	~Model()
+	{
+	}
+
+private:
+	std::string directory;
+	std::vector<Texture> textures_loaded;
+
+	void processNode(aiNode *node, const aiScene *scene)
+	{
+		for (int i = 0; i < node->mNumMeshes; i++)
+		{
+			aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+			meshes.push_back(processMesh(mesh, scene));
+		}
+		for (int i = 0; i < node->mNumChildren; i++)
+		{
+			processNode(node->mChildren[i], scene);
+		}
+	}
+
+	Mesh processMesh(aiMesh *mesh, const aiScene *scene)
+	{
+		//vertex
+		std::vector<Vertex> verVec;
+		std::vector<GLuint> indexVec;
+		std::vector<Texture> texVec;
+
+		for (int i = 0; i < mesh->mNumVertices; i++)
+		{
+			Vertex vertex;
+			glm::vec3 vector;
+			vector.x = mesh->mVertices[i].x;
+			vector.y = mesh->mVertices[i].y;
+			vector.z = mesh->mVertices[i].z;
+			vertex.Position = vector;
+
+			vector.x = mesh->mNormals[i].x;
+			vector.y = mesh->mNormals[i].y;
+			vector.z = mesh->mNormals[i].z;
+			vertex.Normal = vector;
+
+			if (mesh->mTextureCoords[0]) // 网格是否有纹理坐标？
+			{
+				glm::vec2 vec;
+				vec.x = mesh->mTextureCoords[0][i].x;
+				vec.y = mesh->mTextureCoords[0][i].y;
+				vertex.TexCoord = vec;
+			}
+			else
+				vertex.TexCoord = glm::vec2(0.0f, 0.0f);
+
+			verVec.push_back(vertex);
+		}
+
+		//index
+		for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+		{
+			aiFace face = mesh->mFaces[i];
+			for (unsigned int j = 0; j < face.mNumIndices; j++)
+				indexVec.push_back(face.mIndices[j]);
+		}
+
+		//material
+		if (mesh->mMaterialIndex >= 0)
+		{
+			aiMaterial *mat = scene->mMaterials[mesh->mMaterialIndex];
+			std::vector<Texture> diffuseVec = loadMaterialTextures(mat, aiTextureType_DIFFUSE, "texture_diffuse");
+			texVec.insert(texVec.end(), diffuseVec.begin(), diffuseVec.end());
+			std::vector<Texture> specularVec = loadMaterialTextures(mat, aiTextureType_SPECULAR, "texture_specular");
+			texVec.insert(texVec.end(), specularVec.begin(), specularVec.end());
+		}
+
+		return Mesh(verVec, texVec, indexVec);
+	}
+	
+	std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName)
+	{
+		std::vector<Texture> textures;
+		for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
+		{
+			aiString str;
+			mat->GetTexture(type, i, &str);
+			bool skip = false;
+			for (unsigned int j = 0; j < textures_loaded.size(); j++)
+			{
+				//if (std::strcmp(texture_loaded[j].path.data(), str.C_Str()) == 0)
+				if (std::strcmp(textures_loaded[j].path.C_Str(), str.C_Str()) == 0)
+				{
+					textures.push_back(textures_loaded[j]);
+					skip = true;
+					break;
+				}
+			}
+			if (!skip)
+			{
+				Texture texture;
+				texture.id = TextureFromFile(str.C_Str(), directory);
+				texture.type = typeName;
+				texture.path = str;
+				textures.push_back(texture);
+				textures_loaded.push_back(texture);
+			}
+		}
+		return textures;
+	}
+};
+
+unsigned int TextureFromFile(const char *path, const std::string &directory, bool gamma)
+{
+	std::string filename = std::string(path);
+	filename = directory + '/' + filename;
+
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+
+	int width, height, nrComponents;
+	unsigned char *data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
+	if (data)
+	{
+		GLenum format;
+		if (nrComponents == 1)
+			format = GL_RED;
+		else if (nrComponents == 3)
+			format = GL_RGB;
+		else if (nrComponents == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		//glBindTexture(GL_TEXTURE_2D, 0);
+		stbi_image_free(data);
+	}
+	else
+	{
+		std::cout << "Texture failed to load at path: " << path << std::endl;
+		stbi_image_free(data);
+	}
+
+	return textureID;
+}
+
