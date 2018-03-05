ecfb532a576125a43b50507e18d914fe9c637549
GitHub-bigT <564241248@qq.com>
2018-03-05 19:46:36 +0800

    msaa

--- afresh/MSAA/simple-camera/main.cpp
+++ afresh/MSAA/simple-camera/main.cpp
@@ -84,6 +84,8 @@ int main()
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//MSAA
+	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow *window = glfwCreateWindow(windowWidth, windowHeight, "bigT-camera", NULL, NULL);
@@ -110,6 +112,8 @@ int main()
 	// configure global opengl state
 	// -----------------------------
 	glEnable(GL_DEPTH_TEST);
+	//MSAA
+	glEnable(GL_MULTISAMPLE);
 
 	// build and compile shaders
 	// -------------------------
@@ -132,7 +136,7 @@ int main()
 
 		// render
 		// ------
-		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// don't forget to enable shader before setting uniforms
